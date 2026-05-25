982ada286f57ffb3b720c189f7cc3f9cff6612ca
Charles Farris <charlesfarris71@gmail.com>
2026-05-25 10:11:24 -0600

    Use correct variables for Hono context

--- src/features/add-todo/addToDoApi.tsx
+++ src/features/add-todo/addToDoApi.tsx
@@ -1,9 +1,8 @@
-import type { AppVariables } from "@shared/appVariables.ts";
+import type { AuthenticatedAppVariables } from "@shared/appVariables.ts";
 import { Hono } from "hono";
 
-export const addToDoApi = new Hono<{ Variables: AppVariables }>().post(
-  "/",
-  (c) => {
-    return c.json({}, 200);
-  },
-);
+export const addToDoApi = new Hono<{
+  Variables: AuthenticatedAppVariables;
+}>().post("/", (c) => {
+  return c.json({}, 200);
+});
