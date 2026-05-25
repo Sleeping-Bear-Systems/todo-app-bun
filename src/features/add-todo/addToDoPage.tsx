fbc0c7028ec80c7a4ff76232f83705ed31a249e5
Charles Farris <charlesfarris71@gmail.com>
2026-05-24 20:04:32 -0600

    Add JWT payload shape validation
    - Update Hono application variables
    - Remove old utility methods

--- src/features/add-todo/addToDoPage.tsx
+++ src/features/add-todo/addToDoPage.tsx
@@ -1,14 +1,12 @@
-import type { AppVariables } from "@shared/appVariables.ts";
+import type { AuthenticatedAppVariables } from "@shared/appVariables.ts";
 import { Page } from "@shared/page.tsx";
 import { pageJwtMiddleware } from "@shared/pageJwtMiddleware.ts";
-import { getUsernameFromJwtPayload } from "@shared/utility.ts";
 import { Hono } from "hono";
 
-export const addToDoPage = new Hono<{ Variables: AppVariables }>()
+export const addToDoPage = new Hono<{ Variables: AuthenticatedAppVariables }>()
   .use("/", pageJwtMiddleware)
   .get("/", (c) => {
-    const username = getUsernameFromJwtPayload(c.var.jwtPayload);
-
+    const username = c.var.validatedJwtPayload.preferred_username;
     return c.html(
       <Page type="authenticated" currentPath={c.req.path} username={username}>
         <h1>Add ToDo</h1>
--- src/shared/pageJwtMiddleware.ts
+++ src/shared/pageJwtMiddleware.ts
@@ -1,24 +1,28 @@
 import { createMiddleware } from "hono/factory";
 import { jwt } from "hono/jwt";
-import type { AppVariables } from "./appVariables.ts";
+import type { AuthenticatedAppVariables } from "./appVariables.ts";
+import { jwtPayloadSchema } from "./authentication.ts";
 import { pageRoutes } from "./pageRoutes.ts";
 
-export const pageJwtMiddleware = createMiddleware<{ Variables: AppVariables }>(
-  async (c, next) => {
-    const appConfig = c.var.appConfig;
-    const verifyJwt = jwt({
-      secret: appConfig.jwt.secret,
-      cookie: appConfig.jwt.cookieName,
-      alg: "HS256",
-    });
+export const pageJwtMiddleware = createMiddleware<{
+  Variables: AuthenticatedAppVariables;
+}>(async (c, next) => {
+  const appConfig = c.var.appConfig;
+  const verifyJwt = jwt({
+    secret: appConfig.jwt.secret,
+    cookie: appConfig.jwt.cookieName,
+    alg: "HS256",
+  });
 
-    try {
-      await verifyJwt(c, async () => {});
-    } catch {
-      return c.redirect(pageRoutes.LOGIN);
-    }
+  try {
+    await verifyJwt(c, async () => {});
+    const validatedJwtPayload = jwtPayloadSchema.parse(c.var.jwtPayload);
+    c.set("validatedJwtPayload", validatedJwtPayload);
+  } catch (e) {
+    console.log(e);
+    return c.redirect(pageRoutes.LOGIN);
+  }
 
-    await next();
-    return;
-  },
-);
+  await next();
+  return;
+});
